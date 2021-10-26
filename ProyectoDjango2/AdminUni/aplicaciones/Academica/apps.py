d4e7720e83117d140e51c1dd596666e999ff262a
Daniel Valverde Quijano <dannielxdx@outlook.com>
2021-10-26 14:33:20 -0500

    se crea ejemplo de Academia para practicar Django

---
+++ ProyectoDjango2/AdminUni/aplicaciones/Academica/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+#name = 'nombredEcARPETA.NombreDeModulo'
+
+class AcademicaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'aplicaciones.Academica'
