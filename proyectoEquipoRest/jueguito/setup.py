ab8e3443aa298e28846cb333f9c24b48eb93ddc2
dannielXdx <dannielxdx@outlook.com>
2021-10-29 20:10:41 -0500

    Se se agrega la carpeta de proyecto en equipo para respaldarlo

---
+++ proyectoEquipoRest/jueguito/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+<Author>
+
+
+<Start Date>
+    29 Oct 2021
+
+<Description>
+    Deployment script for the pldapi standalone application
+
+<Usage>
+
+    Refer to the documentation hosted at [redacted] for
+    instructions on how to use this file
+
+"""
+from setuptools import setup
+
+setup(
+    name="jueguito",
+    version="0.0.0a",
+    author="",
+    author_email="",
+    description=(""),
+    packages=["jueguito"],
+    install_requires=[],
+)
